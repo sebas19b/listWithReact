dd36061084e20d0538d8d60c20287f67c4c4eb8a
Sebastian Barreto <sdbarretof11@gmail.com>
2025-01-27 10:55:37 -0500

    Creamos un array con las tareas y sus propiedades

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoAddTaskButton } from './TodoAddTaskButton';
 import { TodoItems } from './TodoItem';
+import React from 'react';
 import './App.css';
+
+const listTodos = [
+{text: "Sacar la basura", completed: false},
+{text: "Aprender react, del curso de platzi", completed: false},
+{text: "desayunar", completed: true},
+{text: "ir al trabajo", completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter/>
+      <TodoCounter completed={7} total={10} />
       <TodoSearch/>
 
       <TodoList>
-        <TodoItems/>
-        <TodoItems/>
-        <TodoItems/>
+        {listTodos.map(todo => (
+          <TodoItems 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <TodoAddTaskButton/>
       
-    </div>
+    </React.Fragment>
   );
 }
 
