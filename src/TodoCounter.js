a6f9873b447c29307d0b853f406db56f592e4f02
Sebastian Barreto <sdbarretof11@gmail.com>
2025-01-27 10:57:35 -0500

    Se modifico la estructura del encabezado para hacerlo mas dinamico

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,6 @@
-function TodoCounter () {
+function TodoCounter ( {completed, total} ) {
     return (
-        <h2>You have successfull 4 of 5 task</h2>
+        <h2>You have successfull {completed} of {total} task</h2>
     )
 }
 
