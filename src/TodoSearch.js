d8ed32c49d84f564f485dfd570c25b9cad90edd2
Sebastian Barreto <sdbarretof11@gmail.com>
2025-02-03 21:41:57 -0500

    le agregamos el evento para que en capture lo ingresado en la caja de texto

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,14 @@ import './css/TodoSearch.css'
 function TodoSearch() {
     return(
         <div className='containerSearch'>
-            <input className='inputAddTask' type="text" placeholder="Write task"/>
+            <input className='inputAddTask' type="text" placeholder="Write task"
+                onChange={
+                    (event)=>{
+                        console.log("Escribiste")
+                        console.log(event.target.value)
+                    }
+                }
+            />
         </div>
     )
 }
