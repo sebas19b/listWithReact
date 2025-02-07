6117f0feacd0a285876e5e9dd68181322f3e9c3c
Sebastian Barreto <sdbarretof11@gmail.com>
2025-02-06 21:37:13 -0500

    Agregue una serie de estados, todos - searchValue y constantes para acceder a informacion, taskcompleted - totalTask - taskSerched

--- src/App.js
+++ src/App.js
@@ -14,14 +14,33 @@ const listTodos = [
 ]
 
 function App() {
+
+  const [todos, setTodos] = React.useState(listTodos)
+
+  const taskCompleted = listTodos.filter(listTodos => listTodos.completed).length
+
+  const totalTask = listTodos.length
+
+  const [searchValue, setSearchValue] = React.useState("")
+
+  console.log(searchValue)
+  
+  const taskSearched = listTodos.filter((todos) => {
+    return todos.text.toLowerCase().includes(searchValue.toLowerCase())
+  })
+
   return (
     <React.Fragment>
 
-      <TodoCounter completed={7} total={10} />
-      <TodoSearch/><TodoAddTaskButton/>
+      <TodoCounter completed={taskCompleted} total={totalTask} />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <TodoAddTaskButton/>
 
       <TodoList>
-        {listTodos.map(todo => (
+        {taskSearched.map(todo => (
           <TodoItems 
             key={todo.text} 
             text={todo.text} 
